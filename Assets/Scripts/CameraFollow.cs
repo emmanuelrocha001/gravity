8b9ad9b7a2d72e9832ebaab86b117cf0aeb23809
Emmanuel Rocha <emmanuelrocha001@gmail.com>
2018-12-21 10:37:47 -0600

    Camera Follower implemented

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+  public Transform target;
+  public float smoothSpeed;
+  public Vector3 offset;
+  public Vector3 desiredPosition;
+  public Vector3 smoothedPosition;
+  // Use this for initialization
+  void Start()
+  {
+    smoothSpeed = .25f;
+    offset = new Vector3(0f, 2f, -10f);
+  }
+
+  // Update is called once per frame
+  void LateUpdate()
+  {
+    desiredPosition = target.position + offset;
+    smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+    transform.position = smoothedPosition;
+
+  }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Player : MonoBehaviour
+{
+
+  public GameObject currentPlanet;
+  public Vector3 targetPlanet;
+  public float speed;
+
+
+
+
+  // Start is called before the first frame update
+  void Start()
+   {
+       
+   }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (currentPlanet != null)
+    {
+      targetPlanet = new Vector3(currentPlanet.GetComponent<Transform>().position.x, currentPlanet.GetComponent<Transform>().position.y);
+      //Debug.Log(targetPlanet.position);
+      transform.position = Vector3.MoveTowards(transform.position, targetPlanet, Time.deltaTime * speed);
+
+    }
+     
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    if(other.CompareTag("planet"))
+    {
+      Debug.Log("Collision entered!");
+      currentPlanet = other.gameObject;
+    }
+  }
+
+    
+}
